0835ce932c32f7c5114abbff3930572cddf775dc
saurabhthecodewizard <skmahajan4998@gmail.com>
2024-03-04 23:13:35 +0530

    header and hero animation

---
+++ client/app/utils/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+    return (
+        <>
+        <title>Acadia</title>
+        <meta name='viewport' content='width=device-width initial-scale=1' />
+        <meta name='description' content='ACADIA is a learning platform' />
+        <meta name='keywords' content='Programming, Back-end, Front-end, Databases, Cloud Computing' />
+        </>
+    )
+};
+
+export default Header;
