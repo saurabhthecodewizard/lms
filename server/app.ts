db93aa762ff8630b7068e2b41498ea47985d856d
saurabhthecodewizard <skmahajan4998@gmail.com>
2024-03-06 02:12:24 +0530

    cors error fixed

--- server/app.ts
+++ server/app.ts
@@ -18,7 +18,8 @@ app.use(cookieParser());
 
 // cross origin resource sharing
 app.use(cors({
-    origin: process.env.ORIGIN
+    origin: ["http://localhost:3000"],
+    credentials: true
 }));
 
 // routes
